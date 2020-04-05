7dca23c47307e3ebcf8e19c607f95185632b1a71
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-04-05 18:12:56 -0400

    add get_api_football_json_from_url.R

---
+++ src/sports/soccer/get_api_football_json_from_url.R
@@ -0,0 +1,12 @@
+get_api_football_json_from_url <- function(url){
+	settings <- get_project_settings()
+
+	key <- read_file('src/sports/soccer/api.key')
+	headers <- c(key)
+	names(headers) <- 'X-RapidAPI-Key'
+
+	response <- httr::GET(url, add_headers(.headers = headers))
+	rawJson <- httr::content(response, as = 'text')
+	json <- jsonlite::fromJSON(rawJson)$api
+	return (json)
+}
