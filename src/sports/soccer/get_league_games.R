1ecaf160926ae74ecab309c59653c6f46b600c80
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-04-16 00:00:19 -0400

    add GSSD prediction code

--- src/sports/soccer/get_league_games.R
+++ src/sports/soccer/get_league_games.R
@@ -19,12 +19,20 @@ get_league_games <- function(leagueId){
 	btPredictions <- data.frame(rawGames['fixture_id'], btPredictionModel$predictGameByIds(rawGames$homeTeam$team_name, rawGames$awayTeam$team_name))
 
 	toorPredictionModel <- SportPredictR::team_ols_rating(gameIds =  rawGames$fixture_id,
-														 homeTeamIds = rawGames$homeTeam$team_name,
-														 awayTeamIds = rawGames$awayTeam$team_name,
-														 homeScores = ifelse(rawGames$status == 'Match Finished', rawGames$goalsHomeTeam, NA),
-														 awayScores = ifelse(rawGames$status == 'Match Finished', rawGames$goalsAwayTeam, NA),
-														 isNeutralSite = F)
+														  homeTeamIds = rawGames$homeTeam$team_name,
+														  awayTeamIds = rawGames$awayTeam$team_name,
+														  homeScores = ifelse(rawGames$status == 'Match Finished', rawGames$goalsHomeTeam, NA),
+														  awayScores = ifelse(rawGames$status == 'Match Finished', rawGames$goalsAwayTeam, NA),
+														  isNeutralSite = F)
 	toorPredictions <- data.frame(rawGames['fixture_id'], toorPredictionModel$predictGameByIds(rawGames$homeTeam$team_name, rawGames$awayTeam$team_name))
+
+	gssdPredictionModel <- SportPredictR::team_ols_rating(gameIds =  rawGames$fixture_id,
+														  homeTeamIds = rawGames$homeTeam$team_name,
+														  awayTeamIds = rawGames$awayTeam$team_name,
+														  homeScores = ifelse(rawGames$status == 'Match Finished', rawGames$goalsHomeTeam, NA),
+														  awayScores = ifelse(rawGames$status == 'Match Finished', rawGames$goalsAwayTeam, NA),
+														  isNeutralSite = F)
+	gssdPredictions <- data.frame(rawGames['fixture_id'], gssdPredictionModel$predictGameByIds(rawGames$homeTeam$team_name, rawGames$awayTeam$team_name))
 
 	tempGames <- rawGames %>% inner_join(btPredictions, by = c('fixture_id' = 'fixture_id')) %>% filter(rawGames$status != 'Match Postponed')
 	gameTimes <- ymd_hms(tempGames$event_date) %>% with_tz('America/Detroit')
