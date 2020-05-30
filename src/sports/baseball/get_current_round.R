8b41f03376ae84fceb4fe13d35e95db10c1053fa
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-05-30 12:34:59 -0400

    implement baseball get_current_round

--- src/sports/baseball/get_current_round.R
+++ src/sports/baseball/get_current_round.R
@@ -1,3 +1,24 @@
 get_current_round <- function(leagueId){
-	return(NULL)
+	source('src/sports/baseball/get_league_games.R')
+
+	games <- get_league_games(leagueId)
+	gameDates <- games$Round %>% stringr::str_replace_all('_', '-') %>% ymd() %>% unique()
+
+	currentDate <- date(lubridate::now())
+
+	maxDate <- max(gameDates)
+	isAllPast <- maxDate < currentDate
+	if(isAllPast){
+		return(maxDate)
+	}
+
+	minDate <- min(gameDates)
+	isAllFuture <- minDate > currentDate
+	if(isAllFuture){
+		return(minDate)
+	}
+
+	nextGame <- (games %>% filter(is.na(HomeScore) & is.na(AwayScore)) %>% arrange(GameTime) %>% slice(1))[1,]
+	currentRound <- nextGame$Round
+	return(currentRound)
 }
