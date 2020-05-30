ad8f52f9377e3b13ce1bc820bbbcc37435e183f6
Zachary P. Malosh, MS, MBA <maloshzp@gmail.com>
2020-05-30 11:32:43 -0400

    add baseball get_country_groups

--- src/sports/baseball/get_country_groups.R
+++ src/sports/baseball/get_country_groups.R
@@ -1,3 +1,25 @@
+library(tidyverse)
 get_country_groups <- function(){
-	return(NULL)
+	countries <- read_csv('data/baseball/countries.csv',
+						  col_types = cols(
+						  	CountryId = col_integer(),
+						  	CountryName = col_character(),
+						  	CountryAbbr = col_character(),
+						  	FlagUrl = col_character(),
+						  	ApiBaseballCountryName = col_character()
+						  ))
+
+	countryGroups <- read_csv('data/baseball/countryGroups.csv',
+							  col_types = cols(
+							  	GroupName = col_character(),
+							  	GroupPriority = col_integer(),
+							  	CountryPriority = col_integer(),
+							  	CountryCode = col_character()
+							  ))
+
+	result <- countryGroups %>%
+		inner_join(countries, by = c('CountryCode' = 'CountryAbbr')) %>%
+		select(GroupName, GroupPriority, CountryCode, CountryName, CountryPriority, FlagUrl)
+
+	return(result)
 }
